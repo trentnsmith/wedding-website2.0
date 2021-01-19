3dd90e2ce7a570bff572d28b5f2da480d1b5b38d
Trent Smith <trentnsmith@gmail.com>
2021-01-18 18:50:15 -0600

    skeleton for wedding party layout

--- src/components/WeddingParty/WeddingParty.js
+++ src/components/WeddingParty/WeddingParty.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import './WeddingParty.css';
 
 class WeddingParty extends Component {
     render() { 
         return (
+            <section className="wedding-party">
             <div>
                 <h1>Wedding Party</h1>
             </div>
+            <div className="container-party">
+                <div className="party party-bride">
+                    <ul>
+                        <li>Maid of Honor</li>
+                        <li>Bridesmaid</li>
+                        <li>Bridesmaid</li>
+                        <li>Bridesmaid</li>
+                        <li>Bridesmaid</li>
+                        <li>Bridesmaid</li>
+                        <li>Bridesmaid</li>
+                    </ul>
+                </div>
+                <div className="party party-groom">
+                    <ul>
+                        <li>Best Man</li>
+                        <li>Groomsman</li>
+                        <li>Groomsman</li>
+                        <li>Groomsman</li>
+                        <li>Groomsman</li>
+                        <li>Groomsman</li>
+                        <li>Groomsman</li>
+                    </ul>
+                </div>
+            </div>
+            </section>
         );
     }
 }
