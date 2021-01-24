a6b83c81fefa4548b49d97be69edfc8aa54e4583
Trent Smith <trentnsmith@gmail.com>
2021-01-23 19:26:38 -0600

    added registry photos/no link yet

--- src/components/Registry/Registry.js
+++ src/components/Registry/Registry.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import './Registry.css';
+
+import CB from '../images/places/candblogo.jpg';
+import Target from '../images/places/retailers-target-logo.png';
 
 class Registry extends Component {
     render() { 
         return (
-            <div>
+            <section className="reg-section">
                 <h1>Registry</h1>
-            </div>
+                <div className="reg-container">
+                    <div className="reg reg-target">
+                        <img className="img-target" src={Target} alt="Target Registry" />
+                    </div>
+                    <div className="reg reg-cb">
+                        <img className="img-cb" src={CB} alt="CrateAndBarrel Registry" />
+                    </div>
+                </div>
+            </section>
         );
     }
 }
