adb81044fa0556e1952ab4070af345d333709066
Trent Smith <trentnsmith@gmail.com>
2021-01-25 21:58:00 -0600

    layout for ourstory section complete

--- src/components/OurStory/OurStory.js
+++ src/components/OurStory/OurStory.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './OurStory.css';
 
+import FirstPhoto from '../images/story/first.jpg';
+import Scriptown from '../images/story/oneyear.jpg';
+import Hawaii from '../images/story/hawaiiwaterfall.jpg';
+import Engagement from '../images/story/engagement.jpg';
 
 
 class Story extends Component {
@@ -9,9 +13,59 @@ class Story extends Component {
     render() { 
        
         return (
-            <section className="container">
+            <section className="story-section">
+                <div className="story-overlay">
                 <h1>Our Story</h1>
-                    
+                    <div className="story-container">
+                        <div className="story-block story-begin">
+                            <div className="photo-container">
+                                <img className="story-photo first" src={FirstPhoto} alt="Our First" />
+                            </div>
+                            <div className="story-details">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                        </div>
+
+                        <div className="story-block story-date">
+                            <div className="story-details">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                            <div className="photo-container">
+                                <img className="story-photo first" src={Scriptown} alt="Scriptown" />
+                            </div>
+                        </div>
+                            
+                        <div className="story-block story-between">
+                            <div className="photo-container">
+                                <img className="story-photo first" src={Hawaii} alt="Waterfall" />
+                            </div>
+                            <div className="story-details">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                        </div>
+
+                        <div className="story-block story-engagement">
+                            <div className="story-details">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                            <div className="photo-container">
+                                <img className="story-photo first" src={Engagement} alt="Engagement" />
+                            </div>
+                        </div>
+                    </div>
+                </div>              
             </section>
         );
     }
