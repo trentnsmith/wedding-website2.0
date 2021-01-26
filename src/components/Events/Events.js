c1d089793d4e7d665e46c0882de7a90a29155b64
Trent Smith <trentnsmith@gmail.com>
2021-01-25 22:18:45 -0600

    styling adjustments

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -13,10 +13,11 @@ class Events extends Component {
                         <div className="wedding-event">
                             <div className="information info-church">
                                 <h2>Wedding</h2>
-                                <h3>St Mary's Church</h3>
+                                
                             </div>
                         </div>
                         <div>
+                            <h3>St Mary's Church</h3>
                             <p>
                                 <a href="https://www.google.com/maps/place/St+Mary's+Church/@41.6627652,-91.5341255,17z/data=!3m1!4b1!4m5!3m4!1s0x87e441f0444cad4b:0xef7278d071e93764!8m2!3d41.6627652!4d-91.5319368" target="_blank" rel="noreferrer" >
                                     228 E Jefferson St, Iowa City, IA 52245
@@ -31,10 +32,11 @@ class Events extends Component {
                         <div className="reception-event">
                             <div className="information info-rec">
                                 <h2>Reception</h2>
-                                <h3>Walker Homestead</h3>
+                                
                             </div>
                         </div>
                         <div>
+                            <h3>Walker Homestead</h3>
                             <p>
                                 <a href="https://www.google.com/maps/place/Walker+Homestead/@41.6522183,-91.6598851,17z/data=!3m1!4b1!4m5!3m4!1s0x87e446157bbd2477:0x48ca5efd2061a638!8m2!3d41.6522183!4d-91.6576964" target="_blank" rel="noreferrer" >
                                     3867 James Ave SW, Iowa City, IA 52246
