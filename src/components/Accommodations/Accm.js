0fffa360f23101fb3d77f75e4c622fcd7382743c
Trent Smith <trentnsmith@gmail.com>
2021-02-01 22:31:28 -0600

    styling

--- src/components/Accommodations/Accm.js
+++ src/components/Accommodations/Accm.js
@@ -17,8 +17,10 @@ class Accm extends Component {
                             
                         </div>
                         <div className="info-block">
-                            <h2>Hotel</h2>
-                            <h3>The Highlander Hotel</h3>
+                            
+                            <h2>
+                                <a href="https://www.highlanderhotel.us/" target="_blank">The Highlander Hotel</a>
+                            </h2>
                             <a href="https://www.google.com/maps/place/The+Highlander+Hotel/@41.6884292,-91.4975532,17z/data=!3m1!4b1!4m8!3m7!1s0x87e46983f8ad28ed:0x361441f31deb06ef!5m2!4m1!1i2!8m2!3d41.6884292!4d-91.4953645" target="_blank" rel="noreferrer" >
                                 2525 Highlander Pl, Iowa City, IA 52245
                             </a>< br />
@@ -42,7 +44,7 @@ class Accm extends Component {
                             <p className="updates">Please check back for updates!</p>
                             <p className="updates">Once the formal invitations are sent, we would like 
                             to get a head count of who will be using the transportation service. 
-                            This will be aded to the RSVP!</p>
+                            This will be added to the RSVP!</p>
                         </div>
                     </div>
                     <div className="hotel hotel-iowa">
@@ -52,12 +54,24 @@ class Accm extends Component {
                         <div className="info-block">
                             <h2>Things To Do In The Area</h2>
                             <ul className="to-do">
-                                <li>Terry Trueblood Recreation Area</li>
-                                <li>Downtown Iowa City</li>
-                                <li>Brown Deer Golf</li>
-                                <li>Big Grove Brewery</li>
-                                <li>Backpocket Brewery</li>
-                                <li>30 Hop American Grill w/ Rooftop Patio</li>
+                                <li>
+                                    <a href="https://www.icgov.org/terry-trueblood-recreation-area" target="_blank">Terry Trueblood Recreation Area</a>
+                                </li>
+                                <li>
+                                    <a href="https://downtowniowacity.com/experience-downtown/" target="_blank">Downtown Iowa City</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.browndeergolf.org/510/Brown-Deer-Golf-Club" target="_blank">Brown Deer Golf Course</a>
+                                </li>
+                                <li>
+                                    <a href="https://biggrovebrewery.com/" target="_blank">Big Grove Brewery</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.backpocketbrewing.com/" target="_blank">Backpocket Brewery</a>
+                                </li>
+                                <li>
+                                    <a href="http://30hop.com/irl-coralville/" target="_blank">30 Hop American Grill w/ Rooftop Patio</a>
+                                </li>
                             </ul>
                         </div>
                     </div>
