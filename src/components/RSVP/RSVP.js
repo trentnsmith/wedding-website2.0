76e4ab57f51137f2e05ed4dcc108793ccf26556f
Trent Smith <trentnsmith@gmail.com>
2021-06-26 21:38:50 -0500

    add rsvp form link and update to RSVP section

--- src/components/RSVP/RSVP.js
+++ src/components/RSVP/RSVP.js
@@ -8,7 +8,18 @@ class RSVP extends Component {
                     <h1>RSVP</h1>
                 </div>
                 <div className="updates">
-                    <p>Check back after you receive the formal inivation!</p>
+                    <p>Please RSVP by July 14, 2021</p>
+                    <p>
+                        <a href="https://trent-jean.wedsites.com/rsvp" target="_blank" rel="noreferrer">
+                            RSVP Form
+                        </a>
+                    </p>
+                    <p>
+                        *If you would like to invite additional people that are not included in the RSVP please reach out to us via email.
+                    </p>
+                    <p>Trent: trentnsmith@gmail.com</p>
+                    <p>Jean: jean.louise.larew.623@gmail.com</p>
+
                 </div>
             </section>
         );
